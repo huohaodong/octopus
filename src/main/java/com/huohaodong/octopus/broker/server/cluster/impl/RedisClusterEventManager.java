485c9122a6c58517014f172be7483126a1744238
Huo Haodong <huohaodong.io@gmail.com>
2023-03-20 13:31:58 +0800

    添加 Redis 集群桥接接口

--- src/main/java/com/huohaodong/octopus/broker/server/cluster/impl/RedisClusterEventManager.java
+++ src/main/java/com/huohaodong/octopus/broker/server/cluster/impl/RedisClusterEventManager.java
@@ -3,11 +3,32 @@ package com.huohaodong.octopus.broker.server.cluster.impl;
 import com.huohaodong.octopus.broker.server.cluster.ClusterEventManager;
 import com.huohaodong.octopus.broker.server.cluster.ClusterMessageIdentity;
 import io.netty.handler.codec.mqtt.MqttMessage;
+import org.springframework.data.redis.connection.Message;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.listener.adapter.MessageListenerAdapter;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.stereotype.Component;
 
-public class RedisClusterEventManager implements ClusterEventManager {
+@Component
+public class RedisClusterEventManager extends MessageListenerAdapter implements ClusterEventManager {
+
+    StringRedisTemplate redisTemplate;
+
+    public RedisClusterEventManager(RedisConnectionFactory connectionFactory) {
+        this.redisTemplate.setConnectionFactory(connectionFactory);
+        this.redisTemplate.setValueSerializer(new Jackson2JsonRedisSerializer<>(String.class));
+        this.redisTemplate.afterPropertiesSet();
+    }
+
+    @Override
+    public void onMessage(Message message, byte[] pattern) {
+
+    }
+
     @Override
     public void broadcast(ClusterMessageIdentity identity, MqttMessage message) {
-
+        redisTemplate.convertAndSend("", message);
     }
 
     @Override
