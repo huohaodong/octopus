8ca4765f1cc1e840c1c5ef22b4c58138e6a7d845
Huo Haodong <huohaodong.io@gmail.com>
2023-04-11 22:35:30 +0800

    添加 session service 实现

--- src/main/java/com/huohaodong/octopus/broker/service/session/impl/SessionServiceImpl.java
+++ src/main/java/com/huohaodong/octopus/broker/service/session/impl/SessionServiceImpl.java
@@ -1,48 +1,58 @@
 package com.huohaodong.octopus.broker.service.session.impl;
 
 import com.huohaodong.octopus.broker.persistence.entity.Session;
+import com.huohaodong.octopus.broker.persistence.repository.SessionRepository;
 import com.huohaodong.octopus.broker.service.session.SessionService;
 import io.netty.channel.Channel;
+import io.netty.channel.ChannelFutureListener;
+import jakarta.annotation.Nonnull;
+import lombok.RequiredArgsConstructor;
+import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Service;
+import org.springframework.util.ConcurrentReferenceHashMap;
 
 import java.util.Optional;
 
 @Service
+@RequiredArgsConstructor
 public class SessionServiceImpl implements SessionService {
 
+    private final ConcurrentReferenceHashMap<String, Channel> clientIdToChannelMap = new ConcurrentReferenceHashMap<>();
+
+    private final SessionRepository sessionRepository;
 
     @Override
     public Channel getChannelByClientId(String clientId) {
-        return null;
+        return clientIdToChannelMap.get(clientId);
     }
 
     @Override
     public void addChannel(String clientId, Channel channel) {
-
+        clientIdToChannelMap.put(clientId, channel);
     }
 
     @Override
     public void closeChannel(Channel channel) {
-
+        channel.closeFuture().addListener((ChannelFutureListener) future -> clientIdToChannelMap.remove(channel));
     }
 
     @Override
     public void putSession(Session session) {
-
+        sessionRepository.save(session);
     }
 
     @Override
     public Optional<Session> getSession(String brokerId, String clientId) {
-        return Optional.empty();
+        return sessionRepository.findByBrokerIdAndClientId(brokerId, clientId);
     }
 
     @Override
     public void removeSession(String brokerId, String clientId) {
-
+        sessionRepository.deleteByBrokerIdAndClientId(brokerId, clientId);
     }
 
     @Override
     public boolean containsSession(String brokerId, String clientId) {
-        return false;
+        return sessionRepository.existsByBrokerIdAndClientId(brokerId, clientId);
     }
 }
