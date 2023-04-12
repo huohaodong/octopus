63e9d38011cb7f05014e31e309981e045ec53f68
Huo Haodong <huohaodong.io@gmail.com>
2023-04-12 15:56:35 +0800

    完善订阅管理

--- src/main/java/com/huohaodong/octopus/broker/persistence/repository/SubscriptionRepository.java
+++ src/main/java/com/huohaodong/octopus/broker/persistence/repository/SubscriptionRepository.java
@@ -4,13 +4,16 @@ import com.huohaodong.octopus.broker.persistence.entity.Subscription;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface SubscriptionRepository extends JpaRepository<Subscription, Long> {
-    List<Subscription> findByBrokerIdAndClientIdAndTopic(String brokerId, String clientId, String topic);
+    Optional<Subscription> findByBrokerIdAndClientIdAndTopic(String brokerId, String clientId, String topic);
 
     void deleteByBrokerIdAndClientIdAndTopic(String brokerId, String clientId, String topic);
 
     List<Subscription> findAllByBrokerIdAndClientId(String brokerId, String clientId);
 
     void deleteAllByBrokerIdAndClientId(String brokerId, String clientId);
+
+    boolean existsByBrokerIdAndClientIdAndTopic(String brokerId, String clientId, String topic);
 }
