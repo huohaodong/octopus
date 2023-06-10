2a77b73c68e503de2b78db214354bdcfdd5585b5
Huo Haodong <huohaodong.io@gmail.com>
2023-06-10 20:34:47 +0800

    完善遗嘱消息逻辑

--- octopus-broker/src/main/java/com/huohaodong/octopus/broker/protocol/mqtt/MqttPacketDispatcher.java
+++ octopus-broker/src/main/java/com/huohaodong/octopus/broker/protocol/mqtt/MqttPacketDispatcher.java
@@ -1,14 +1,20 @@
 package com.huohaodong.octopus.broker.protocol.mqtt;
 
+import com.huohaodong.octopus.broker.config.BrokerProperties;
 import com.huohaodong.octopus.broker.protocol.mqtt.handler.*;
+import com.huohaodong.octopus.common.persistence.service.message.MessageService;
 import io.netty.channel.ChannelHandler;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
 import io.netty.handler.codec.UnsupportedMessageTypeException;
 import io.netty.handler.codec.mqtt.*;
+import io.netty.handler.timeout.IdleState;
+import io.netty.handler.timeout.IdleStateEvent;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
+
+import static com.huohaodong.octopus.broker.protocol.mqtt.Constants.CHANNEL_ATTRIBUTE_CLIENT_ID;
 
 @Slf4j(topic = "MQTT_DISPATCHER")
 @RequiredArgsConstructor
@@ -34,6 +40,10 @@ public class MqttPacketDispatcher extends SimpleChannelInboundHandler<MqttMessag
     private final PingHandler pingHandler;
 
     private final DisconnectHandler disconnectHandler;
+
+    private final MessageService messageService;
+
+    private final BrokerProperties brokerProperties;
 
     @Override
     protected void channelRead0(ChannelHandlerContext ctx, MqttMessage msg) throws Exception {
@@ -75,6 +85,20 @@ public class MqttPacketDispatcher extends SimpleChannelInboundHandler<MqttMessag
             default:
                 ctx.channel().close();
                 throw new UnsupportedMessageTypeException(msg.decoderResult().cause(), MqttMessageType.class);
+        }
+    }
+
+    @Override
+    public void userEventTriggered(ChannelHandlerContext ctx, Object evt) throws Exception {
+        if (evt instanceof IdleStateEvent idleStateEvent) {
+            String clientId = ctx.channel().attr(CHANNEL_ATTRIBUTE_CLIENT_ID).get();
+            if (idleStateEvent.state() == IdleState.ALL_IDLE) {
+                log.info("heartbeat timeout, close channel");
+                messageService.getWillMessage(brokerProperties.getId(), clientId).ifPresent(willMessage -> publishHandler.sendPublishMessage(willMessage.getTopic(), willMessage.getQos(), willMessage.getPayload()));
+                disconnectHandler.doProcess(ctx, null);
+            }
+        } else {
+            super.userEventTriggered(ctx, evt);
         }
     }
 }
